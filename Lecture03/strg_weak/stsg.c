8445ad9e201e3384ad6e1f459728ce219ba5930f
amolgaj <98952903+amolgaj@users.noreply.github.com>
2022-02-05 20:03:53 +0530

    Strong_weak_linking
    Created Strong and weak symbols and linked and generate exe.

---
+++ Lecture03/strg_weak/stsg.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int fun();
+int x=4,y=5;
+int main(){
+
+fun();
+printf("x = %d\n",x);
+printf("y = %d\n",y);
+return 0 ;
+}
---
+++ Lecture03/strg_weak/weak.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+float x;
+int fun()
+{
+
+x = 45;
+return 0;
+
+}
+
